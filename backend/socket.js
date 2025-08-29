74377c60e9e7ddd211b4722f3fd463a2c01af241
Nakul-coder-2003 <125133233+Nakul-coder-2003@users.noreply.github.com>
2025-08-29 14:26:40 +0530

    Update socket.js

--- backend/socket.js
+++ backend/socket.js
@@ -8,7 +8,7 @@ const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
     origin: "https://social-media-v9zj.onrender.com",
-    credentials: true,
+    methods:["GET","POST"]
   },
 });
 
