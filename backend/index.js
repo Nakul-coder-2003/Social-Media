e688ab543605796ed0f2af1baab22fa8b304c9c4
Nakul-coder-2003 <125133233+Nakul-coder-2003@users.noreply.github.com>
2025-08-29 13:59:50 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ dotenv.config();
 const port = 8000;
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://social-media-v9zj.onrender.com",
     credentials:true
 }))
 app.use(express.json())
